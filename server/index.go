d4d76a3ed316e0c97cec1aafe34d39b336d89e19
Aek <ekkawin.v@gmail.com>
2021-04-11 21:10:30 +0700

    http request

--- server/index.go
+++ server/index.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+
+	myRouter := mux.NewRouter()
+
+	myRouter.HandleFunc("/aek", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hi my name is aek")
+	}).Methods("GET")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "aek")
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", myRouter)
 }
