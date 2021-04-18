b955ac301c2867024cb512bc1c0c70b86cc18113
Aek <ekkawin.v@gmail.com>
2021-04-18 14:46:59 +0700

    frontend setup

--- server/index.go
+++ server/index.go
@@ -5,9 +5,28 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
+
+	dsn := "host=localhost user=aek password=aek dbname=todo_list port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	fmt.Println(db, "db")
+	fmt.Println(err, "err")
+
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
 
 	myRouter := mux.NewRouter()
 
